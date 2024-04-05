259c1364192beeb603219e1706ef9f938f44a701
Daniel Franca <daniel.pe.15y15@gmail.com>
2024-04-04 22:06:32 -0300

    feat: add distill and generate static site

--- server/settings.py
+++ server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "django_distill",
 ]
 
 MIDDLEWARE = [
@@ -118,9 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+DISTILL_DIR = "distil"
+
 STATIC_URL = "/static/"
 
 STATICFILES_DIR = [BASE_DIR / "core/static"]
+
+STATIC_ROOT = BASE_DIR / "vol" / "web" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- server/urls.py
+++ server/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import HomeView
+from django_distill import distill_path
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
+    distill_path("", HomeView.as_view(), name="home"),
     path("admin/", admin.site.urls),
 ]
